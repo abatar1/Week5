a8ba2eb2e67ae63963632123297359013d30aea9
abatar1 <evgenyhalzov@gmail.com>
2016-10-27 19:06:24 +0600

    new

---
+++ Week5/Processor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week5
+{
+    public class Processor<T, TRequest>
+    {
+        public T Process(TRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+
+        public Func<TRequest, bool> Check;
+        public Func<TRequest, T> Register;
+        public Action<T> Save;
+    }
+}
