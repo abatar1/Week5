6f800bc488427b37a21c81c6fd840bfd63da99fa
abatar1 <evgenyhalzov@gmail.com>
2016-10-27 19:54:23 +0600

    update 1

---
+++ Week5/5_2/TableProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Week5
+{
+    public class TableProcessor : ITableProcessor
+    {
+        public void Get(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertColumn(int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRow(int rowIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(int row, int column, int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
