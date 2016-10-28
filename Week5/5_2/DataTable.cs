38f96409aa2288d80c2d2f633d9f6c321253f568
abatar1 <evgenyhalzov@gmail.com>
2016-10-28 17:45:51 +0600

    4.2
    Очень хочется объеденить содержимое InsertRow и InsertColumn в один
    метод, но я не знаю как.

--- Week5/5_2/DataTable.cs
+++ Week5/5_2/DataTable.cs
@@ -50,6 +50,8 @@ namespace Week5
             {
                 if (columnIndex > columnCount)
                     table[i].AddRange(Enumerable.Repeat(default(TType), columnIndex - columnCount));
+                else
+                    table[i].Insert(columnIndex, default(TType));
             }
             columnCount++;
         }
@@ -59,7 +61,9 @@ namespace Week5
             OnLogger(new LoggerEventArgs("InsertRow"));
 
             if (rowIndex > rowCount)
-                table.AddRange(Enumerable.Repeat(new List<TType>(), rowIndex - rowCount));                       
+                table.AddRange(Enumerable.Repeat(new List<TType>(), rowIndex - rowCount));    
+            else                   
+                table.Insert(rowIndex, new List<TType>());
             rowCount++;
         }
 
