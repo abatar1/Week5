df8aa553659fa3819f62c7287a479491d14dd1b1
abatar1 <evgenyhalzov@gmail.com>
2016-10-28 16:30:52 +0600

    update 3.1(3 task)

--- Week5/5_2/TableProcessor.cs
+++ Week5/5_2/TableProcessor.cs
@@ -1,27 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Week5
 {
-    public class TableProcessor : ITableProcessor
+    public class DataTable<T>
     {
-        public void Get(int row, int column)
+        private List<List<T>> table;       
+
+        private int rowCount = 0;
+        private int columnCount = 0;
+
+        public event EventHandler<LoggerEventArgs> LoggerEvent;
+
+        private bool ValueInRange(int row, int column)
         {
-            throw new NotImplementedException();
+            OnLogger(new LoggerEventArgs("ValueInRange"));
+
+            return (row < rowCount && column < columnCount) ? true : false;
+        }
+
+        public T Get(int row, int column)
+        {
+            OnLogger(new LoggerEventArgs("Get"));
+
+            if (ValueInRange(row, column))
+                return table[row][column];
+            else
+                throw new IndexOutOfRangeException();
         }
 
         public void InsertColumn(int columnIndex)
         {
-            throw new NotImplementedException();
+            OnLogger(new LoggerEventArgs("InsertColumn"));
+
+            columnCount++;
+            if (rowCount == 0) rowCount++;
+            foreach (var cell in table)
+            {
+                cell.Insert(columnIndex, default(T));
+            }
         }
 
         public void InsertRow(int rowIndex)
         {
-            throw new NotImplementedException();
+            OnLogger(new LoggerEventArgs("InsertRow"));
+
+            rowCount++;
+            table.Insert(rowIndex, default(List<T>));
         }
 
-        public void Put(int row, int column, int value)
+        public void Put(int row, int column, T value)
         {
-            throw new NotImplementedException();
+            OnLogger(new LoggerEventArgs("Put"));
+
+            if (ValueInRange(row, column))
+                table[row][column] = value;
+            else
+                throw new IndexOutOfRangeException();
+        }
+
+        protected virtual void OnLogger(LoggerEventArgs e)
+        {
+            LoggerEvent?.Invoke(this, e);
         }
     }
 }
