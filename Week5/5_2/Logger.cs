07d96d97465d5a1777f12418dcaaba0aa2b8ca02
abatar1 <evgenyhalzov@gmail.com>
2016-10-28 16:28:57 +0600

    update 3(3 task)

---
+++ Week5/5_2/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week5
+{
+    public class Logger
+    {
+        public static void MethodInvoked(object sender, LoggerEventArgs e)
+        {
+            Console.WriteLine(e.ToString());
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Week5/5_2/LoggerEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Week5
+{
+    public class LoggerEventArgs
+    {
+        private string methodName;
+        private DateTime time;
+
+        public LoggerEventArgs(string methodName)
+        {
+            this.methodName = methodName;
+            this.time = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return methodName.ToString() + " at " + time.ToString();
+        }
+    }
+}
